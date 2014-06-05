3eebc7088ce81c08bf8cf54d729ff6c8d343992e
Dave <davewilliams1978@yahoo.com>
2014-06-04 20:31:35 -0400

    added comments and made minor changes

--- hash1.c
+++ hash1.c
@@ -2,39 +2,39 @@
 
 int main (void)
 {
-    // offer the user four options. 
+    // offer the user five options. 
     while (true)
     {
         printf("Please choose an option:\n\n");
         printf("0 - Quit\n");
-        printf("1 - Load the Dictionary\n");
+        printf("1 - Load the Dictionary (recommended to be done first)\n");
         printf("2 - Print an individual Letter of the Dictionary\n");
         printf("3 - Print the entire Dictionary\n");
         printf("4 - Check to see if a specific word is in the Dictionary\n");
         printf("\n"); 
-        int option = GetInt();
+        int option = GetInt(); // get user's decision
 
         switch (option)
         {
-            // quit
+            // quit: free table and say "Goodbye!"
             case 0: free_table(); printf("\n\nGoodbye!\n\n"); return 0;
 
-            // load dictionary
+            // load dictionary and inform user that it has been done.
             case 1: load(); printf("\nThe dictionary has now been uploaded.\n\n"); break;
 
             // print an individual section ("Letter") of the dictionary.
-            case 2: printf("\nWhich letter of the dictionary would you like to print?\n\n");
-                    char* c = GetString();
-                    int h = hash(c);
-                    print_i(h); free(c); break;
+            case 2: printf("\nWhich letter of the dictionary would you like to print?\n\n"); // prompt user
+                    char* c = GetString(); // get string from user
+                    int h = hash(c); // determine hash key
+                    print_i(h); free(c); break; // print table for letter & free memory
                     
-            case 3: print_all(); break;
+            case 3: print_all(); break; // print the entire F%@#$!ing Dictionary
             
-            case 4: printf("\nWhich word would you like to check?\n\n");
-                    char* w = GetString(); 
-                    check(w); free(w); break;                    
+            case 4: printf("\nWhich word would you like to check?\n\n"); // prompt user
+                    char* w = GetString(); // get string from user
+                    check(w); free(w); break; // check word and free memory                    
                     
-            default: printf("\nNot a valid option.\n\n"); break;
+            default: printf("\nNot a valid option.\n\n"); break; // invalid option
         }                
     }
 }    
--- hash1.h
+++ hash1.h
@@ -13,6 +13,8 @@
  * separate chaining (linked lists).
  *
  */
+
+// libraries to include, superflous or not. 
 #include <cs50.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -22,25 +24,38 @@
 #include <ctype.h>
 
 
-// define node.  Arbitrarily setting max word length to 56 char.
-typedef struct node {char word[56]; struct node* next;} node;
+// define node.  Arbitrarily setting max word length to 50 characters.
+typedef struct node {char word[50]; struct node* next;} node;
 
 // initialize table array
 node* table[26];
 // declare functions
-int hash (char* string);
+void free_table (void);
 void load (void);
 void print_i (int i);
 void print_all (void);
-void free_table (void);
 void check (char* w);
+int hash (char* string);
 
-// frees table
+/**
+ *
+ *frees table
+ *
+ */
+ 
 void free_table (void)
 {
+    // free every "i"th spot of the table
     for (int i = 0; i <= 25; i++) {free(table[i]);}
-}    
-// loads dictionary
+}
+
+    
+/**
+ *
+ * loads dictionary
+ *
+ */
+ 
 void load (void)
 {
     // Open dictionary    
@@ -80,78 +95,107 @@ void load (void)
                inserted.  */ 
             table[k] = new_node;
         }
-    free(new_node);                     
+    free(new_node);  // free temporary node                   
     }
-    fclose(file);
+    fclose(file); // close file
 }
 
+/**
+ *
+ * prints all the words of a given letter of the dictionary.
+ * also prints out the total number of words for the given letter.
+ *
+ */
+ 
 void print_i (int i)
 {
+    // check to see if the dictionary has been loaded and respond accordingly
     if (table[i] == NULL) {printf("Dictionary has not been loaded yet!\n\n");}
     
-    int word_counter = 0;
-    int x = i;
-    node* current = table[i]; 
-    int letter = x + 65;
-    printf("\nDictionary - Letter: %c\n\n", (char) letter);
-    while (current->word != NULL) 
+    int word_counter = 0; // initialize word counter
+    
+    node* current = table[i]; // set temporary node to head of the list
+    
+    int letter = i + 65; // obtain ASCII letter (Capitial)
+    
+    printf("\nDictionary - Letter: %c\n\n", (char) letter); // print header
+    
+    while (current->word != NULL) // while loop until end of list
     {
-        printf("%s.\n", current->word); 
-        current = current->next;
-        word_counter++;
+        printf("%s.\n", current->word); // print current word
+        current = current->next; // move to next word
+        word_counter++; // increase counter
     }
-    printf("\n");
-    printf("Total Letter %c word count: %i.\n\n", (char) letter, word_counter);
-    free(current);        
+    // print final word count 
+    printf("\nTotal Letter %c word count: %i.\n\n", (char) letter, word_counter);
+    free(current); // free temporary node       
 }
 
+/**
+ *
+ * print the entire F%@#$!ing Dictionary, along with headers and word
+ * counts for each letter, and a total word count
+ *
+ */
+ 
 void print_all (void) 
 {
-    int word_counter = 0;
-    printf("\nDictionary - Complete:\n\n");
+    int word_counter = 0; // initialize total word counter
     
-    for (int i = 0; i <= 25; i++) 
+    printf("\nDictionary - Complete:\n\n"); // print header
+    
+    for (int i = 0; i <= 25; i++) // for loop to access hash table
     {
-        int letter = i + 65;
-        int letter_counter = 0;
-        printf("Dictionary - Letter: %c\n\n", (char) letter); 
-        node* current = table[i];
-        while (current->word != NULL) 
+        int letter = i + 65; // determine current ASCII letter value
+        int letter_counter = 0; // initialize current letter counter
+        printf("Dictionary - Letter: %c\n\n", (char) letter); // print header
+        node* current = table[i]; // set temporary node to the head of list
+        while (current->word != NULL) // while loop until end of list
         {
-            printf("%s.\n", current->word); 
-            current = current->next;
-            word_counter++;
-            letter_counter++;
+            printf("%s.\n", current->word); // print word
+            current = current->next; // move to next word
+            word_counter++; letter_counter++; // update counters
         }
-        printf("\n");
-        printf("Total Letter %c word count: %i.\n\n", (char) letter, letter_counter);
-        free(current);  
+        // print word counter for each letter
+        printf("\nTotal Letter %c word count: %i.\n\n", (char) letter, letter_counter);
+        free(current);  // free temporary node
     }
+    // print total word count for dictionary
     printf("Total Dictionary Word Count: %i.\n\n", word_counter);
 }
 
+/**
+ *
+ * checks a word requested from the user against the dictionary
+ *
+ */
 
 void check(char* w)
 {
+    // allocate memory for a string that will contain the unadulterated word
     char* original = malloc((strlen(w) + 1) * sizeof(char));
-    if (original == NULL) {free(w);}
+    if (original == NULL) {free(w);} // check for NULL
     
-    
+    // convert word to lowercase to compare it to the all lower-care dictionary
     for (int i = 0, n = strlen(w); i < n; i++) {original[i] = w[i]; w[i] = tolower(w[i]);}
     
-    int h = hash(w);
+    int h = hash(w); // hash word to determine hash table access key
     
-    node* current = table[h];
+    node* current = table[h]; // set temporary node to head of list
     
-    while (current->word != NULL && w != NULL)
+    while (current->word != NULL && w != NULL) // NULL check and loop until end of list
     { 
+        // Scenario 1:  Word matches current word in list   
         if (strcmp(w,current->word) == 0) {printf("\n%s is in the Dictionary!\n\n", original); break;}
-        
+        /* Scenario 2: Word does not match current word.
+           Apocryphal Buddhist and/or Confucian proverb:
+           "To advace further, look within" */
         else {current = current->next;}
-    }            
+    }
+    // Inform user that word was not in the dictionary            
     if (current->word == NULL) {printf("\n%s was not in the Dictionary.\n\n", original);}
-    free(current);
-    free(original);    
+    free(current); // free temporary node
+    free(original); // free string containing the originally requested word
 }
   
 /* basic hash function - takes the first letter of each string and returns a 
